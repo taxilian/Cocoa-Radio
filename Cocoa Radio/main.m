4253868e96519f38d69c34eccf6e1468d90f39e9
William Dillon <hpux735@gmail.com>
2012-07-10 14:35:36 -0700

    Added GPL licensing information

--- Cocoa Radio/main.m
+++ Cocoa Radio/main.m
@@ -3,7 +3,7 @@
 //  Cocoa Radio
 //
 //  Created by William Dillon on 6/7/12.
-//  Copyright (c) 2012 Oregon State University (COAS). All rights reserved.
+//  Copyright (c) 2012. All rights reserved. Licensed under the GPL v.2
 //
 
 #import <Cocoa/Cocoa.h>
