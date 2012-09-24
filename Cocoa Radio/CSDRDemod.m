713db2897909c1cfa53503b00af06a3a825719f2
William Dillon <hpux735@gmail.com>
2012-09-24 14:31:50 -0700

    Copied the wideband FM demodulation function contents into the narrowband FM function

--- Cocoa Radio/CSDRDemod.m
+++ Cocoa Radio/CSDRDemod.m
@@ -363,6 +363,12 @@ int gcd(int a, int b) {
 
 - (NSData *)demodulateData:(NSDictionary *)complexInput
 {
+    // Make sure that the temporary arrays are big enough
+    int samples = [complexInput[@"real"] length] / sizeof(float);
+    if ([radioPower length] < (samples * sizeof(float))) {
+        [radioPower setLength:samples * sizeof(float)];
+    }
+    
     // Down convert
     NSDictionary *baseBand;
     baseBand = freqXlate(complexInput, self.centerFreq, self.rfSampleRate);
@@ -371,16 +377,40 @@ int gcd(int a, int b) {
     NSDictionary *filtered;
     filtered = [IFFilter filterDict:baseBand];
     
+    // Get an array of signal power levels for squelch
+    getPower(filtered, radioPower, powerContext, .0001);
+    
     // Quadrature demodulation
+    float dGain = demodGain + (self.rfSampleRate / (2 * M_PI * IFFilter.bandwidth));
     NSMutableData *demodulated;
-    demodulated = (NSMutableData *)quadratureDemod(filtered, demodGain, 0.);
+    demodulated = (NSMutableData *)quadratureDemod(filtered, dGain, 0.);
     
     // Remove any residual DC in the signal
     removeDC(demodulated, &average, .0001);
-
+    
     // Audio Frequency filter
-    NSData *audioFiltered;
-    audioFiltered = [AFFilter filterData:demodulated];
+    NSMutableData *audioFiltered;
+    audioFiltered = (NSMutableData *)[AFFilter filterData:demodulated];
+    
+    // Iterate through the audio and mute sections that are too low
+    // for now, just use a manual squelch threshold
+    
+    const float *powerSamples = [radioPower bytes];
+    float *audioSamples = [audioFiltered mutableBytes];
+    double newAverage = 0;
+    
+    for (int i = 0; i < samples; i++) {
+        double powerSample = powerSamples[i];
+        newAverage += powerSample / (double)samples;
+        
+        bool mute = (powerSample > self.squelch)? NO : YES;
+        float audioSample = audioSamples[i];
+        audioSamples[i] = (mute)? 0. : audioSample;
+    }
+    
+    // Copy average power into the rfPower variable
+    COCOARADIO_DEMODAVERAGE((int)(rfPower * 1000));
+    rfPower = newAverage * 10;
     
     // Rational resampling
     NSData *audio;
